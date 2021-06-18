588d70b29331303e7284774effb33fb58c5ec55d
valentingnt <valentin64.genest@gmail.com>
2021-06-18 15:07:54 +0200

    parsed and show articles title into homepage

---
+++ layout/MainLayout.js
@@ -0,0 +1,9 @@
+const MainLayout = ({ children }) => {
+    return (
+        <>
+            {children}
+        </>
+    );
+}
+
+export default MainLayout;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,52 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import MainLayout from "../layout/MainLayout";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+const Home = ({ data }) => {
+
+  let articles = data.articles
+  console.log(data);
+
   return (
-    <div className={styles.container}>
+    <MainLayout className={styles.container}>
       <Head>
         <title>Dernier Cri News App</title>
         <meta name="Application news pour Dernier Cri" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
-    </div>
-  )
-}
+      <ul>
+        {articles.map((article, index) => {
+          return <li key={index}> {article.title}</li>;
+        })}
+      </ul>
+    </MainLayout>
+  );
+};
+
+
+export const getServerSideProps = async ({}) => {
+  let apiKey = process.env.API_KEY;
+
+  const url =
+    "https://newsapi.org/v2/everything?" +
+    "qInTitle=+tesla&" +
+    "from=2021-06-16&" +
+    "sortBy=popularity&" +
+    `apiKey=${apiKey}`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      props: null,
+    };
+  } else
+    return {
+      props: { data },
+    };
+};
+
+export default Home;
