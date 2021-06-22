80200389e0269194c3088e3e61997a9338c9fd1b
valentingnt <valentin64.genest@gmail.com>
2021-06-22 21:29:16 +0200

    responsive again and again but it's look cool

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -16,50 +16,76 @@ const Menu = () => {
   return (
     <>
       <menu className={styles.menuGrid}>
-          <Link scroll={false} href="/themes/fashion">
-            <a>
-              <div><p>Fashion</p></div>
-            </a>
-          </Link>
-          <Link scroll={false} href="/themes/technology">
-            <a>
-              <div><p>Technology</p></div>
-            </a>
-          </Link>
-          <Link scroll={false} href="/themes/environment">
-            <a>
-              <div><p>Environment</p></div>
-            </a>
-          </Link>
-          <Link scroll={false} href="/themes/politics">
-            <a>
-              <div><p>Politics</p></div>
-            </a>
-          </Link>
-          <Link scroll={false} href="/themes/finance">
-            <a>
-              <div><p>Finance</p></div>
-            </a>
-          </Link>
-          <Link scroll={false} href="/themes/society">
-            <a>
-              <div><p>Society</p></div>
-            </a>
-          </Link>
-          <Link scroll={false} href="/themes/sport">
-            <a>
-              <div><p>Sport</p></div>
-            </a>
-          </Link>
-          <Link scroll={false} href="/themes/art%20culture">
-            <a>
-              <div><p>Culture</p></div>
-            </a>
-          </Link>
+        <Link scroll={false} href="/themes/fashion">
+          <a>
+            <div>
+              <p>Fashion</p>
+            </div>
+          </a>
+        </Link>
+        <Link scroll={false} href="/themes/technology">
+          <a>
+            <div>
+              <p>Technology</p>
+            </div>
+          </a>
+        </Link>
+        <Link scroll={false} href="/themes/environment">
+          <a>
+            <div>
+              <p>Environment</p>
+            </div>
+          </a>
+        </Link>
+        <Link scroll={false} href="/themes/politics">
+          <a>
+            <div>
+              <p>Politics</p>
+            </div>
+          </a>
+        </Link>
+        <Link scroll={false} href="/themes/finance">
+          <a>
+            <div>
+              <p>Finance</p>
+            </div>
+          </a>
+        </Link>
+        <Link scroll={false} href="/themes/society">
+          <a>
+            <div>
+              <p>Society</p>
+            </div>
+          </a>
+        </Link>
+        <Link scroll={false} href="/themes/sport">
+          <a>
+            <div>
+              <p>Sport</p>
+            </div>
+          </a>
+        </Link>
+        <Link scroll={false} href="/themes/art%20culture">
+          <a>
+            <div>
+              <p>Culture</p>
+            </div>
+          </a>
+        </Link>
       </menu>
       <div className={styles.searchArea}>
-        <input placeholder="Rechercher une actualité..." type="text" id="search" className={styles.searchBox}/>
-        <input type="button" value="Search" onClick={searchBox} className={styles.searchBtn} />
+        <input
+          placeholder="Rechercher une actualité..."
+          type="text"
+          id="search"
+          className={styles.searchBox}
+        />
+        <input
+          type="button"
+          value="Search"
+          onClick={searchBox}
+          className={styles.searchBtn}
+        />
       </div>
     </>
   );
